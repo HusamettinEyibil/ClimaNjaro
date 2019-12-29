83d8a780df45ad60784a99f7185ad5dcc664530c
Hüsamettin Eyibil <heyibil76@gmail.com>
2019-12-29 16:06:32 +0300

    Initial commit

---
+++ ClimaNjaro/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ClimaNjaro
+//
+//  Created by Hüsamettin  Eyibil on 28.12.2019.
+//  Copyright © 2019 Hüsamettin  Eyibil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //let gif = UIImage.gif(name: "rain_gif")
+        backgroundImageView.loadGif(name: "rain_gif")
+    }
+
+
+}
+
